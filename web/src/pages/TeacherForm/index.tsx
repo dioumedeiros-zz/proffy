a44b7a6986a0e218ecf54be8c95fca4e582b9e10
Diou Medeiros <diou.182@gmail.com>
2020-08-05 20:35:12 -0300

    chore: Updated TeacherForm

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import PageHeader from '../../components/PageHeader';
 
 const TeacherForm: React.FC = () => {
-  return <div />;
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Que incível que você quer dar aulas." />
+    </div>
+  );
 };
 
 export default TeacherForm;
