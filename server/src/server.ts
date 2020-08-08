e74b29b75af51aa6cac5f3d18a62027d15f95798
Diou Medeiros <diou.182@gmail.com>
2020-08-07 21:50:07 -0300

    chore: Add cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import cors from 'cors';
+import routes from './routes';
 
 const app = express();
+
+app.use(express.json());
+app.use(routes);
+app.use(cors());
 
 app.listen(3333);
